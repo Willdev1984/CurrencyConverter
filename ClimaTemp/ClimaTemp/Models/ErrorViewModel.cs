d6514dc029336af32aed7cccaa295d0978ef259c
Willian Nogueira <101224462+Willdev1984@users.noreply.github.com>
2026-07-22 11:00:07 +0100

    Delete ClimaTemp directory

--- ClimaTemp/ClimaTemp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ClimaTemp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
